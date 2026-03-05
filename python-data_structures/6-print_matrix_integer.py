8c4ae7fe405a03e64f84b5a1bc26634992f952cc
DTounda <d.nanatoun@alustudent.com>
2026-03-05 02:40:01 -0800

    printing integers in a matrix with a new line at the end of each row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for index, number in enumerate(row):
-            if index >= len(row) - 1:
-                print("{:d}".format(number))
-            else:
-                print("{:d}".format(number), end=" ")
+        if row != []:
+            for index, number in enumerate(row):
+                if index >= len(row) - 1:
+                    print("{:d}".format(number))
+                else:
+                    print("{:d}".format(number), end=" ")
+
+        else:
+            print()
